c6894c94046b6c22a5c4181f69656b16318d66eb
x.r.meng <x.r.meng@qq.com>
2017-05-07 17:31:35 +0800

    refact mapper file

--- SellOrder/src/main/java/net/linghou/erp/sellorder/mapper/SellOrderMapper.java
+++ SellOrder/src/main/java/net/linghou/erp/sellorder/mapper/SellOrderMapper.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import net.linghou.erp.sellorder.domain.SalesOrder;
 
 @Repository
-public interface SalesOrderMapper {
+public interface SellOrderMapper {
 	public SalesOrder findOne(Long id );
 	public Long register(SalesOrder order);
 	public int update(SalesOrder order );
